59a1c167bb74068d751ca1fe7407ba3ee2ba6f58
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-19 13:35:15 +0530

    Time: 4 ms (93.61%), Space: 14.6 MB (94.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,40 +11,8 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> mergedValues;
-
-        // Merge the values of both lists into the vector
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                mergedValues.push_back(list1->val);
-                list1 = list1->next;
-            } else {
-                mergedValues.push_back(list2->val);
-                list2 = list2->next;
-            }
-        }
-        while (list1 != nullptr) {
-            mergedValues.push_back(list1->val);
-            list1 = list1->next;
-        }
-
-        while (list2 != nullptr) {
-            mergedValues.push_back(list2->val);
-            list2 = list2->next;
-        }
-        ListNode* mergedList = nullptr;
-        ListNode* current = nullptr;
-        
-        for(int i=0; i<mergedValues.size(); i++){
-            if(mergedList==nullptr){
-                mergedList=new ListNode(mergedValues[i]);
-                current=mergedList;
-            }
-            else{
-                current ->next=new ListNode(mergedValues[i]);
-                current=current->next;
-            }
-        }
-        return mergedList;
+        if(!list1||(list2&&list1->val>list2->val)) swap(list1,list2);
+        if(list1) list1->next= mergeTwoLists(list1->next,list2);
+        return list1;
     }
 };
