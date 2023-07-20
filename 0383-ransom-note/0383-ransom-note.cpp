a4d82d9d6dff378f404ee0cfcca3ed7ec2e5fa6a
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-20 22:03:17 +0530

    Time: 28 ms (32.53%), Space: 8.9 MB (29.80%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> map;
+        unordered_map<char,int> map;
         int i;
         for(i=0;i<magazine.length();i++){
             map[magazine[i]]++;
