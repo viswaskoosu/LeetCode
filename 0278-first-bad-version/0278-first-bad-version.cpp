427fc126f77dfe48f12675888970cb1f1eae8fc9
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-20 20:38:20 +0530

    Time: 3 ms (44.28%), Space: 5.9 MB (42.27%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(n==1) return 1;
+        // if(n==1) return 1;
         long int left=1,right=n;
         while(left<right){
             if(isBadVersion((right+left)/2)) right=left + (right-left)/2;
