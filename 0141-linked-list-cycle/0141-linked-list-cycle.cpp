34dbe8078352ee460029a2d484551bce677a5e6b
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-20 15:56:10 +0530

    Time: 8 ms (81.20%), Space: 8 MB (65.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,27 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* realhead=head;
         if(!head||!head->next) return 0;
-        vector<int> a;
-        while(head){
-            if(isin(a,head,realhead)) return 1;
-            a.push_back(head->val);
-            head=head->next;
-        }
-        return 0;
-    }
-    bool isin(vector<int> a,ListNode* head,ListNode* realhead){
-        ListNode* head1=realhead;
-        // head1=head1->next;
-        int ans=0;
-        for(int j=0;j<a.size();j++){
-            if(a[j]==head->val){
-                if(head1==head){
-                    return 1;
-                }
-            }
-            head1=head1->next;
+        ListNode* slow=head;
+        head=head->next;
+        while(head && head->next){
+            slow=slow->next;
+            head=head->next->next;
+            if(slow==head) return 1;
         }
         return 0;
     }
