8bf6cd2b1d88be81953d9d190536cc1387c6e1e1
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-21 11:22:36 +0530

    Time: 21 ms (66.12%), Space: 19.6 MB (50.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,11 +3,11 @@ public:
     int majorityElement(vector<int>& nums) {
         map<int,int> m;
         for(int i=0;i<nums.size();i++){
-            m[nums[i]]+=1;
-            if(m[nums[i]]>nums.size()/2) return nums[i];
+            m[nums[i]]++;
+            // if(m[nums[i]]>nums.size()/2) return nums[i];
         }
         for( map<int,int>::iterator i=m.begin(); i!=m.end(); ++i){
-            if(i->second>=nums.size()/2) return i->first;
+            if(i->second>nums.size()/2) return i->first;
         }
         return 0;
     }
