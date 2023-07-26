9f2b07c521428c818c6c1966620810c2251e3003
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-07-26 22:23:53 +0530

    Time: 445 ms (16.89%), Space: 71.7 MB (43.7%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> v(k);
         priority_queue<vector<int>> maxheap;
-        for(int i=0;i<points.size();++i){
+        for(int i=0;i<points.size();i++){
             maxheap.push({points[i][0]*points[i][0]+points[i][1]*points[i][1],points[i][0],points[i][1]});
             if(maxheap.size()>k){
                 maxheap.pop();
             }
         }
-        for(int i=0;i<k;++i){
+        for(int i=0;i<k;i++){
             vector<int> top=maxheap.top();
             maxheap.pop();
             v[i]={top[1],top[2]};
