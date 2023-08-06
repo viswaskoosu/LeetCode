6b0cad1dedf4953034cc69e687d8c15c8fa8a33d
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-08-06 17:55:59 +0530

    Time: 2242 ms (5.02%), Space: 18.4 MB (97.74%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<intervals.size()-1;++i){
+            if(intervals[i][1]>=intervals[i+1][0]){
+                if(intervals[i][1]>=intervals[i+1][1]){
+                    intervals.erase(intervals.begin()+i+1);
+                }
+                else{
+                    intervals[i][1]=intervals[i+1][1];
+                    intervals.erase(intervals.begin()+i+1);
+                }
+                i--;
+            }
+        }
+        return intervals;
+    }
+};
