5179484a3f5e2f0682b525d79297297b4f97c0d1
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-08-06 18:03:35 +0530

    Time: 2253 ms (5.02%), Space: 18.5 MB (96.29%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,13 +4,8 @@ public:
         sort(intervals.begin(),intervals.end());
         for(int i=0;i<intervals.size()-1;++i){
             if(intervals[i][1]>=intervals[i+1][0]){
-                if(intervals[i][1]>=intervals[i+1][1]){
-                    intervals.erase(intervals.begin()+i+1);
-                }
-                else{
-                    intervals[i][1]=intervals[i+1][1];
-                    intervals.erase(intervals.begin()+i+1);
-                }
+                intervals[i][1]=max(intervals[i][1],intervals[i+1][1]);
+                intervals.erase(intervals.begin()+i+1);
                 i--;
             }
         }
