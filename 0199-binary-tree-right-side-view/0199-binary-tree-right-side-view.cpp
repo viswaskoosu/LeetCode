fa6597f71b5f12609a8ed67b341cfb21fe9552e1
Koosu Viswas <100070969+viswaskoosu@users.noreply.github.com>
2023-08-14 19:16:37 +0530

    Time: 3 ms (69.18%), Space: 12.1 MB (35.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,26 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> v;
-        func(root, v, 0);
-        vector<int> v1;
-        for (int i = 0; i < v.size(); ++i) {
-            if (!v[i].empty()) {
-                v1.push_back(v[i].back());
+        vector<int> ans;
+        if(!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            TreeNode* t1=q.front();
+            q.pop();
+            ans.push_back(t1->val);
+            if(t1->right) q.push(t1->right);
+            if(t1->left) q.push(t1->left);
+            for(int i=1;i<size;++i){
+                TreeNode* temp=q.front();
+                q.pop();
+                if(temp->right) q.push(temp->right);
+                if(temp->left) q.push(temp->left);
             }
         }
-        return v1;
-    }
-
-    void func(TreeNode* root, vector<vector<int>>& v, int i) {
-        if (!root) return;
-
-        if (i >= v.size()) {
-            v.push_back(vector<int>());
-        }
-        
-        v[i].push_back(root->val);
-        func(root->left, v, i + 1);
-        func(root->right, v, i + 1);
+        return ans;
     }
 };
